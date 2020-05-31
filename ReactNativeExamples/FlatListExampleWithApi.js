f762d2ceccfb78c41533e91dbc26859b19642de2
dmernies-atix <dmernies@atixlabs.com>
2020-05-31 15:40:49 -0300

    feat: Se agrega ActivityIndicator

--- ReactNativeExamples/FlatListExampleWithApi.js
+++ ReactNativeExamples/FlatListExampleWithApi.js
@@ -1,91 +1,24 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * FlatListExampleWithApi
+ * Es un ejemplo de una FlatList que consume una API y le pone un
+ * Spinner.
+ *
+ * Coponentes utilizados:
+ * -FlatList, ActivityIndicator, useState, useEffect,
  *
  * @format
  * @flow strict-local
  */
 
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 
-const hardData = [
-  {
-    id: 1,
-    name: 'Luna Lunita',
-    state: 1,
-  },
-  {
-    id: 2,
-    name: 'La flor más bonita',
-    state: 1,
-  },
-  {
-    id: 3,
-    name: 'Luna Lunita',
-    state: 1,
-  },
-  {
-    id: 4,
-    name: 'Brilla en la noche',
-    state: 1,
-  },
-  {
-    id: 5,
-    name: 'y Brilla de día',
-    state: 1,
-  },
-  {
-    id: 6,
-    name: 'Robocop',
-    state: 1,
-  },
-  {
-    id: 7,
-    name: 'Diego Alejandro',
-    state: 1,
-  },
-  {
-    id: 8,
-    name: 'Nicolás Maximiliano',
-    state: 1,
-  },
-  {
-    id: 9,
-    name: 'Robocop',
-    state: 1,
-  },
-  {
-    id: 10,
-    name: 'Diego Alejandro',
-    state: 1,
-  },
-  {
-    id: 12,
-    name: 'Nicolás Maximiliano',
-    state: 1,
-  },
-  {
-    id: 13,
-    name: 'Robocop',
-    state: 1,
-  },
-  {
-    id: 14,
-    name: 'Diego Alejandro',
-    state: 1,
-  },
-  {
-    id: 15,
-    name: 'Nicolás Maximiliano',
-    state: 1,
-  },
-  {
-    id: 16,
-    name: 'Robocop',
-    state: 1,
-  },
-];
 const FlatListExampleWithApi: () => React$Node = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -99,7 +32,7 @@ const FlatListExampleWithApi: () => React$Node = () => {
       });
   }, []);
   if (loading) {
-    return <Text>Loading</Text>;
+    return <ActivityIndicator size="large" color="#000" />;
   }
   return (
     <View style={styles.container}>
